99cb1d72e39e2192b8dbe959e37aeb16b1b3c932
VNMad <vadiknoname@gmail.com>
2026-07-08 09:40:14 +0200

    Django project change DATABASES

--- config/settings.py
+++ config/settings.py
@@ -83,12 +83,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if env.bool("MYSQL"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": env("MYSQL_NAME"),
+            "USER": env("MYSQL_USER"),
+            "PASSWORD": env("MYSQL_PASSWORD"),
+            "HOST": env("MYSQL_HOST"),
+            "PORT": env("MYSQL_PORT"),
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
@@ -128,21 +140,3 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-if env.bool("MYSQL"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.mysql",
-            "NAME": env("MYSQL_NAME"),
-            "USER": env("MYSQL_USER"),
-            "PASSWORD": env("MYSQL_PASSWORD"),
-            "HOST": env("MYSQL_HOST"),
-            "PORT": env("MYSQL_PORT"),
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
